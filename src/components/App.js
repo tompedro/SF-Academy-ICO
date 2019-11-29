39345f0009ac3e78451c1e76f33d7069365f79b4
tompedro <55707296+tompedro@users.noreply.github.com>
2019-11-29 20:40:30 +0100

    Finishing touch to frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import logo from '../logo.svg';
-import logoT from '../logo-t.svg';
+import logoT from '../logoT.svg';
 import './App.css';
 import Web3 from 'web3'
 import Token from '../abis/TokenMarketplace.json'
@@ -15,7 +14,8 @@ class App extends Component{
                   inputDollars:0,
                   offers:[],
                   inputName :"" , inputPassword :"" , inputSurname : "" , inputMail : "",
-                  logName : "", logPassword : ""};
+                  logName : "", logPassword : "",
+                  init : false, signin : true};
     this.signIn = this.signIn.bind(this)
     this.getInfo = this.getInfo.bind(this)
     this.addDollars = this.addDollars.bind(this)
@@ -23,6 +23,7 @@ class App extends Component{
     this.getAll = this.getAll.bind(this)
     this.buy = this.buy.bind(this)
     this.logIn = this.logIn.bind(this)
+    this.switch = this.switch.bind(this)
   }
 
   callAPI() {
@@ -52,7 +53,7 @@ class App extends Component{
                 "&password=" + this.state.logPassword.toString()
       })
       .then(res => res.text())
-      .then(res => this.setState({ account: res } , () =>{this.getInfo()}))
+      .then(res => this.setState({ account: res } , () =>{this.getInfo();this.setState({init : false})}))
     }
   }
 
@@ -105,6 +106,10 @@ class App extends Component{
     })
   }
 
+  switch(_tag){
+    this.setState({signin : _tag});
+  }
+
   componentDidMount() {
       this.callAPI();
   }/*
@@ -118,76 +123,88 @@ class App extends Component{
       <div>
         <Navbar account={this.state.account} tokens={this.state.tokens} dollars={this.state.dollars}/>
         <div className="container-fluid mt-5">
-          <div className = "row" id = "init">
-          <form onSubmit={(event) => {
-            event.preventDefault()
-            this.signIn()
-            }}>
+          {
+            this.state.init ?
+            <div class ="container">
+              <main role="main" className="col-lg-12 text-center">
+                <h1 style = {{paddingTop : "70px"}}>WELCOME</h1>
+              </main>
+              <div style = {{paddingLeft : "400px" ,paddingTop : "70px"}}>
+                <button className = "btn btn-outline-info" onClick={()=>{this.switch(true);}}>SIGN IN</button>
+                <button style ={{marginLeft : "150px"}}className = "btn btn-outline-info" onClick={()=>{this.switch(false)}}>LOGIN</button>
+              </div>
+              <div style = {{paddingLeft : "400px",paddingTop : "20px"}}className = "row" id = "init">
+                {
+                  this.state.signin ?
+                  <form onSubmit={(event) => {
+                    event.preventDefault()
+                    this.signIn()
+                    }}>
+                      <label for="uname"><b>Username</b></label>
+                      <input style = {{width : "170%"}}className = "form-control" type="text" placeholder="Enter Username" id="uname" onChange={event => this.setState({inputName:event.target.value})}/>
 
-              <label for="uname"><b>Username</b></label>
-              <input className = "form-control" type="text" placeholder="Enter Username" id="uname" onChange={event => this.setState({inputName:event.target.value})}/>
+                      <label for="surname"><b>Surname</b></label>
+                      <input style = {{width : "170%"}} className= "form-control" type="text" placeholder="Enter Surname" id="surname" onChange={event => this.setState({inputSurname:event.target.value})}/>
 
-              <label for="surname"><b>Surname</b></label>
-              <input className = "form-control" type="text" placeholder="Enter Surname" id="surname" onChange={event => this.setState({inputSurname:event.target.value})}/>
+                      <label for="mail"><b>Mail</b></label>
+                      <input style = {{width : "170%"}}className = "form-control" type="text" placeholder="Enter Mail" id="mail" onChange={event => this.setState({inputName:event.target.value})}/>
 
-              <label for="mail"><b>Mail</b></label>
-              <input className = "form-control" type="text" placeholder="Enter Mail" id="mail" onChange={event => this.setState({inputName:event.target.value})}/>
+                      <label for="psw"><b>Password</b></label>
+                      <input style = {{width : "170%"}}className = "form-control" type="password" placeholder="Enter Password" id="psw" onChange={event => this.setState({inputPassword:event.target.value})}/>
 
-              <label for="psw"><b>Password</b></label>
-              <input className = "form-control" type="password" placeholder="Enter Password" id="psw" onChange={event => this.setState({inputPassword:event.target.value})}/>
+                      <button type="submit" className="btn btn-primary" >Sign In</button>
+                  </form>
+                  :
+                  <form onSubmit={(event) => {
+                    event.preventDefault()
+                    this.logIn()
+                    }}>
+                      <label for="uname"><b>Username</b></label>
+                      <input style = {{width : "170%"}}className = "form-control" type="text" placeholder="Enter Username" id="uname" onChange={event => this.setState({logName:event.target.value})}/>
 
-              <button type="submit" className="btn btn-primary" >Sign In</button>
+                      <label for="psw"><b>Password</b></label>
+                      <input style = {{width : "170%"}}className = "form-control" type="password" placeholder="Enter Password" id="psw" onChange={event => this.setState({logPassword:event.target.value})}/>
 
-            </form>
+                      <button type="submit" className="btn btn-primary" >Log In</button>
 
-          <form onSubmit={(event) => {
-            event.preventDefault()
-            this.logIn()
-            }}>
+                  </form>
+                }
+              </div>
+            </div>
+              : 
+            <div className="row" id = "post">
+              <main role="main" className="col-lg-12 d-flex text-center">
 
-              <label for="uname"><b>Username</b></label>
-              <input className = "form-control" type="text" placeholder="Enter Username" id="uname" onChange={event => this.setState({logName:event.target.value})}/>
+                <div className="content mr-auto ml-auto">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={logoT} className="App-logo" alt="logo" />
+                  </a>
 
-              <label for="psw"><b>Password</b></label>
-              <input className = "form-control" type="password" placeholder="Enter Password" id="psw" onChange={event => this.setState({logPassword:event.target.value})}/>
-
-              <button type="submit" className="btn btn-primary" >Log In</button>
-
-            </form>
-          </div>
-          <div className="row" id = "post">
-            <main role="main" className="col-lg-12 d-flex text-center">
-
-              <div className="content mr-auto ml-auto">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logoT} className="App-logo" alt="logo" />
-                </a>
-
-                <h1>Marketplace</h1>
-                <div class="container">
-                  <button className = "btn btn-primary" onClick={this.getAll}>Refresh</button>
-                  <div style = {{paddingLeft : "860px"}} class="row">
-                    <div class = "col">
-                      <input className = "form-control" style = {{marginLeft : "-25px",width : "150%"}} type = "text" onChange={event => this.setState({inputDollars:event.target.value})}/>
-                    </div>
-                    <div class = "col">
-                      <button className = "btn btn-primary" onClick={this.addDollars}>Add dollars</button>
+                  <h1>Marketplace</h1>
+                  <div class="container">
+                    <button className = "btn btn-primary" onClick={this.getAll}>Refresh</button>
+                    <div style = {{paddingLeft : "860px"}} class="row">
+                      <div class = "col">
+                        <input className = "form-control" style = {{marginLeft : "-25px",width : "150%"}} type = "text" onChange={event => this.setState({inputDollars:event.target.value})}/>
+                      </div>
+                      <div class = "col">
+                        <button className = "btn btn-primary" onClick={this.addDollars}>Add dollars</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </main>
-            <Main offers = {this.state.offers}
-                  sell = {this.sell}
-                  buy = {this.buy}
-                  getAll = {this.getAll}
-                  getInfo = {this.getInfo}/>
+              </main>
+              <Main offers = {this.state.offers}
+                    sell = {this.sell}
+                    buy = {this.buy}
+                    getAll = {this.getAll}
+                    getInfo = {this.getInfo}/>
+            </div>
+          } 
           </div>
-        </div>
-        
       </div>
     );
   }
